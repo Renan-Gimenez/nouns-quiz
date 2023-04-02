3dad948bc5878637dc8ca930c70d95b549564e79
Renan Gimenez <renangiimenez@gmail.com>
2023-04-02 13:25:58 -0300

    add questionCounter component

---
+++ src/app/components/QuestionCount.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+import { RiQuestionLine } from "react-icons/ri";
+
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  margin: 8px;
+  margin-right: auto;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translate(16px, 16px);
+
+  svg {
+    color: #808080;
+  }
+
+  span {
+    color: #808080;
+    font-size: 12px;
+    font-weight: bold;
+  }
+`;
+
+export default function QuestionCount(props: any) {
+  return (
+    <StyledDiv>
+      <RiQuestionLine />
+      <span>{`${props.currentQuestion}/20`}</span>
+    </StyledDiv>
+  );
+}
