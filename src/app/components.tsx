ab3464ba8aff97ed33133a8a13fdb40d5714f7dd
Renan Gimenez <renangiimenez@gmail.com>
2023-04-02 13:20:11 -0300

    add home page

---
+++ src/app/components.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+
+export const StyledDiv = styled.div`
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 48px;
+
+  border: 1px solid red;
+
+  .buttons {
+    display: flex;
+    flex-direction: column;
+
+    gap: 16px;
+  }
+`;
+
+export const StyledButton = styled(Link)`
+  width: 160px;
+
+  font-size: 16px;
+  font-weight: 500;
+
+  padding: 10px 20px;
+
+  border: none;
+  outline: none;
+  border-radius: 99px;
+
+  background-color: #f1f1f1;
+  text-decoration: none;
+
+  transition: 200ms;
+  text-align: center;
+
+  :hover {
+    transform: scale(1.05);
+  }
+`;
